0a347e31831b2994173f455fc07490a86d23e73c
‘terupro’ <terupro911@gmail.com>
2022-08-30 11:28:42 +0900

    array.ts is done

---
+++ src/array.ts
@@ -0,0 +1,13 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+let numbers2: Array<number> = [1, 2, 3];
+
+let strings: string[] = ["Tokyo", "Osaka", "Kyoto"];
+
+let nijigenHairetu: number[][] = [
+  [50, 100],
+  [150, 300],
+];
+
+let hairetsu: (number | boolean | string)[] = [1, false, "Japan"];
